cd566ea1b1e66dce989aa10b43ac8b9e54e189b3
Marilyn TEUSCHER <marilyn.teuscher@he-arc.ch>
2025-04-29 11:41:56 +0200

    first commit

---
+++ meteo-service/src/main/java/ch/hearc/jee2024/meteoservice/MeteoServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee2024.meteoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoServiceApplication.class, args);
+    }
+
+}
---
+++ meteo-service/src/test/java/ch/hearc/jee2024/meteoservice/MeteoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee2024.meteoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
