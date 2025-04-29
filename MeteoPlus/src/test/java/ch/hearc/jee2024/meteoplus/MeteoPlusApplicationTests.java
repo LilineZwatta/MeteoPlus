7f734134e6de76b549a86a964fcaaa6a7b777f41
Sberthoud <131236793+Sberthoud@users.noreply.github.com>
2025-04-29 10:41:51 +0200

    create IntelliJ project

---
+++ MeteoPlus/src/test/java/ch/hearc/jee2024/meteoplus/MeteoPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee2024.meteoplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
