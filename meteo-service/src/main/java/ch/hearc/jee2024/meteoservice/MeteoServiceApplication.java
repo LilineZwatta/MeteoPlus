f997ec12219477a625acdc3c0d67438ec035f152
Marilyn TEUSCHER <marilyn.teuscher@he-arc.ch>
2025-05-15 09:20:40 +0200

    init projetct pom.xml and run

--- meteo-service/src/main/java/ch/hearc/jee2024/meteoservice/MeteoServiceApplication.java
+++ meteo-service/src/main/java/ch/hearc/jee2024/meteoservice/MeteoServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeteoServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("Before Start");
         SpringApplication.run(MeteoServiceApplication.class, args);
-    }
+        System.out.println("After Start");
 
+    }
 }
--- stat-service/src/main/java/ch/hearc/jee2024/statservice/StatServiceApplication.java
+++ stat-service/src/main/java/ch/hearc/jee2024/statservice/StatServiceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StatServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("Before Start");
         SpringApplication.run(StatServiceApplication.class, args);
-    }
+        System.out.println("After Start");
 
+    }
 }
+
